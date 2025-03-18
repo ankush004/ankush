a1c09bac4875b22424aa28a07ca0d3711d9053a1
ankush004 <71ankushnayak@gmail.com>
2025-03-18 20:58:49 +0530

    jay jagganath

--- frontend/app/src/components/Navbar.tsx
+++ frontend/app/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-brand">MyApp</Link>
+        <Link to="/" className="navbar-brand">Deepfake</Link>
         <div className="navbar-links">
           <Link to="/login" className="navbar-link">Login</Link>
           <Link to="/signup" className="navbar-button">Sign Up</Link>
